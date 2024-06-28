3e3f3f4e60d8e094916a276a0f80b67b406e4d39
uraltaf <mte.altaf@gmail.com>
2024-06-28 13:25:46 +0500

    updated

--- 01_Database_Agent/database_agent.py
+++ 01_Database_Agent/database_agent.py
@@ -5,8 +5,8 @@ import os
 import pandas as pd
 
 #client = ChatGroq()
-GROQ_API_KEY = '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'
-#os.environ["GROQ_API_KEY']= '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'
+GROQ_API_KEY = ''
+#os.environ["GROQ_API_KEY']= ''
 #Api_key= os.getenv('GROQ_API_KEY')
 # model = ChatGroq(api_key=GROQ_API_KEY)
 
@@ -88,38 +88,38 @@ df.to_sql(
 )
 
 MSSQL_AGENT_PREFIX = """
-you are an agent designed to iteract with SQL database.
 
+You are an agent designed to interact with a SQL database.
 ## Instructions:
-- Given aninput question, create a systacticallyt correct {dialect} query
+- Given an input question, create a syntactically correct {dialect} query
 to run, then look at the results of the query and return the answer.
-- unless the user specifies a specfic number of examples they wish to
+- Unless the user specifies a specific number of examples they wish to
 obtain, **ALWAYS** limit your query to at most {top_k} results.
 - You can order the results by a relevant column to return the most
 interesting examples in the database.
 - Never query for all the columns from a specific table, only ask for
 the relevant columns given the question.
 - You have access to tools for interacting with the database.
-- You MUST double check your query before executing it. If you get an error 
-while executing a query, rewrite the query and try again.
-- Do NOT make any DML statements (INSERT, UPDATE, DELETE, DROP, etc.)
+- You MUST double check your query before executing it.If you get an error
+while executing a query,rewrite the query and try again.
+- DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP etc.)
 to the database.
-- Do NOT MAKE UP AN ANSWER OR USE PRIOR KNOWLEDGE, ONLY USE THE RESULTS
+- DO NOT MAKE UP AN ANSWER OR USE PRIOR KNOWLEDGE, ONLY USE THE RESULTS
 OF THE CALCULATIONS YOU HAVE DONE.
-- YOur response should be in Markdown. However, **when running a SQL Query
+- Your response should be in Markdown. However, **when running  a SQL Query
 in "Action Input", do not include the markdown backticks**.
-Those are only for formatting the response, not for executing the commands.
+Those are only for formatting the response, not for executing the command.
 - ALWAYS, as part of your final answer, explain how you got to the answer
-on a section that starts with: "Explanation:". Include the SQL query as part
-of the explanation section.
+on a section that starts with: "Explanation:". Include the SQL query as
+part of the explanation section.
 - If the question does not seem related to the database, just return
 "I don\'t know" as the answer.
-- Only use the below tools. Only use the information returned by the 
+- Only use the below tools. Only use the information returned by the
 below tools to construct your query and final answer.
-- Do not make up table names, only use the tables returned by any of the 
+- Do not make up table names, only use the tables returned by any of the
 tools below.
 
-##Tools:
+## Tools:
 
 """
 
@@ -127,12 +127,15 @@ tools below.
 
 
 MSSQL_AGENT_FORMAT_INSTRUCTIONS = """
+
+## Use the following format:
+
 Question: the input question you must answer.
-Thought: you should always think what to do.
-Action: the action to take, should be on of [{tool_names}].
+Thought: you should always think about what to do.
+Action: the action to take, should be one of [{tool_names}].
 Action Input: the input to the action.
 Observation: the result of the action.
-...(this Thought/Action/Action Input/Observation can repeat N times)
+... (this Thought/Action/Action Input/Observation can repeat N times)
 Thought: I now know the final answer.
 Final Answer: the final answer to the original input question.
 
@@ -140,30 +143,29 @@ Example of Final Answer:
 <=== Beginning of example
 
 Action: query_sql_db
-Action: Input:
+Action Input: 
 SELECT TOP (10) [death]
-FROM covidtracking
+FROM covidtracking 
 WHERE state = 'TX' AND date LIKE '2020%'
 
 Observation:
 [(27437.0,), (27088.0,), (26762.0,), (26521.0,), (26472.0,), (26421.0,), (26408.0,)]
-Thought: I now know the final answer
+Thought:I now know the final answer
 Final Answer: There were 27437 people who died of covid in Texas in 2020.
 
-
 Explanation:
-I queried the 'covidtracking' table for the 'death' column where the state 
-is 'TX' and the datae starts with '2020'. The query returned a list of tuple 
-with the number of deaths for each day in 2020. To answer the question, 
+I queried the `covidtracking` table for the `death` column where the state
+is 'TX' and the date starts with '2020'. The query returned a list of tuples
+with the number of deaths for each day in 2020. To answer the question,
 I took the sum of all the deaths in the list, which is 27437.
 I used the following query
 
-'''sql
-SELECT[death] FROM covidtracking WHERE state = 'TX' AND  date LIKE '2020'"
-
+```sql
+SELECT [death] FROM covidtracking WHERE state = 'TX' AND date LIKE '2020%'"
+```
 ===> End of Example
-"""
 
+"""
 
 db = SQLDatabase.from_uri(f'sqlite:///{database_file_path}')
 toolkit = SQLDatabaseToolkit(db=db, llm= model)
@@ -185,5 +187,5 @@ agent_executor_SQL = create_sql_agent(
 )
 
 
-response = model.invoke(QUESTION)
+response = agent_executor_SQL.invoke(QUESTION)
 print(response)
